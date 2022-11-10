d924526bf2e27c89d0fdbdac8a7a4aa3acd67d6b
yoselinsoarez <yoselinsoarez93@gmail.com>
2022-11-10 16:24:34 -0300

    react-router

---
+++ src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>welcome to login page</div>
+  )
+}
+
+export default LoginPage
---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <Link to= '/'>Home page</Link>
+            </li>
+            <li>
+                <Link to= '/login page'>Login</Link> 
+            </li>
+            <li>
+                <Link to= '/Page not found'>Error 404</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
